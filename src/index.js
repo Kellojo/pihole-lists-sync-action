2ef71d33a32c28950f8d7706ff84b642acc53e6d
Kellojo <12833426+Kellojo@users.noreply.github.com>
2025-10-01 21:11:56 +0200

    Initial login

--- src/index.js
+++ src/index.js
@@ -12,6 +12,49 @@ async function run() {
 
     core.info(`Pi-hole URL: ${piholeUrl}`);
     core.info(`Blocklist File: ${blocklistFile}`);
+
+    core.info(`Authenticating with Pi-hole...`);
+    const response = await fetch(`${piholeUrl}/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ password: piholePassword }),
+    });
+
+    if (!response.ok) {
+      throw new Error(
+        `Authentication failed with status: ${response.status} - ${response.statusText}`
+      );
+    }
+
+    const { sid } = await response.json();
+    core.info(`Authentication successful`);
+    core.info("");
+
+    core.info(`Fetching blocklists via API`);
+
+    const blocklistResponse = await fetch(`${piholeUrl}/admin/lists`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        sid: sid,
+      },
+    });
+    if (!blocklistResponse.ok) {
+      throw new Error(
+        `Failed to fetch blocklists with status: ${blocklistResponse.status} - ${blocklistResponse.statusText}`
+      );
+    }
+
+    const { lists } = await blocklistResponse.json();
+    core.info(`Fetched ${lists.length} blocklists from Pi-hole`);
+
+    lists.forEach((list) => {
+      core.info(`- ${list.address}`);
+    });
+
+    core.info("");
   } catch (error) {
     // Log the error and fail the action
     core.error("Error occurred:", error.message);
